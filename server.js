05c236867c13403732c7b671031e1d03aa423898
James H <nus4xd@gmail.com>
2023-06-30 20:07:05 +0700

    newest

--- server.js
+++ server.js
@@ -18,20 +18,30 @@ app.get('/', (req, res) => {
 
 app.post('/compile', (req, res) => {
 	// capture user input
-	const input = req.body.input;
+	const compiler = req.body.compiler.toLowerCase();
+	if (!compiler.startsWith('gcc')
+	&& !compiler.startsWith('clang')) {
+		res.send('Selected compiler is unavailable! Available compilers: gcc, clang.');
+		return;
+	}
 	const flag = req.body.flag;
-	// process user input
-	const output = compile(input, flag);
+	if (!flag.match(/^[- _+=()0-9A-Za-z]*$/)) {
+		res.send('Passing illegal characters as flags! Only use characters in [- _+=()0-9A-Za-z].')
+		return;
+	}
+	const text = req.body.text;
+	// process user text
+	const output = compile(compiler, flag, text);
 	// return output to user
 	res.send(output);
 });
 
-function compile(input, flag) {
+function compile(compiler, flag, text) {
 	try {
-		// compile user input with shell
-		cp.execSync(`printf "%s" "${input}" | gcc ${flag} -Werror -x c -`);
+		// compile user text with shell
+		cp.execSync(`printf "%s" "${text}" | ${compiler} ${flag} -Werror -x c -`);
 		// return compiler output
-		return 'Compiled successfully!'
+		return 'Compiled successfully!';
 	} catch (error) {
 		return error.message;
 	}
