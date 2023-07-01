cca234bcdaf316c2f841c8fb796017e5d69f85e7
James H <nus4xd@gmail.com>
2023-07-01 20:28:01 +0700

    newest

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // @ts-check
+"use strict";
 const port = 3000;
 const express = require('express');
 const app = express();
@@ -14,12 +15,10 @@ app.listen(port, () => {
 });
 
 app.get('/', (req, res) => {
-	// render html
 	res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
 app.post('/compile', (req, res) => {
-	// capture user input
 	const flag = req.body.flag;
 	if (!flag.match(/^[- _+=0-9A-Za-z]*$/)) {
 		res.send('Passing illegal characters as flags! Only use characters in [- _+=0-9A-Za-z].');
@@ -27,25 +26,22 @@ app.post('/compile', (req, res) => {
 	}
 	const compiler = req.body.compiler.toLowerCase();
 	const text = req.body.text;
-	// process user text
 	const output = compile(compiler, flag, text);
-	// return output to user
 	res.send(output);
 });
 
 function compile(compiler, flag, text)
 {
 	try {
-		// compile user text with shell
 		cp.execSync(`printf "%s" "${text}" | ${compiler} ${flag} -Werror -fsyntax-only -x c -`);
-		// return compiler output
 		return 'Compiled successfully!';
 	} catch (error) {
-		// return error.message;
 		return '<br>Compilation failed!<br>' + String(error.message)
+			// remove newlines for regex
 			.replace(/\n/g, '')
 			// only show compiler warnings
 			.replace(/^.*?-fsyntax-only -x c -/, '')
+			// add newlines
 			.replace(/<stdin>:/g, '<br>');
 	}
 }
