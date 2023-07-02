da3eb2ef32c27e3095f7e81aa1e06318c663f4f6
James H <nus4xd@gmail.com>
2023-07-02 16:39:32 +0700

    newest

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // @ts-check
 "use strict";
 const port = 3000;
-const express = require('express');
+const express = require("express");
 const app = express();
-const cp = require('child_process');
-const path = require('path');
+const cp = require("child_process");
+const path = require("path");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,15 +14,15 @@ app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
 });
 
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'public', 'index.html'));
-})
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
-app.post('/compile', (req, res) => {
+app.post("/compile", (req, res) => {
 	/** @type {string} */
 	const flag = req.body.flag;
 	if (!flag.match(/^[- _+=0-9A-Za-z]*$/g)) {
-		res.send('Passing illegal characters as flags!<br>Only use characters in [- _+=0-9A-Za-z].');
+		res.send("Passing illegal characters as flags!<br>Only use characters in [- _+=0-9A-Za-z].");
 		return;
 	}
 	/** @type {string} */
@@ -40,26 +40,28 @@ app.post('/compile', (req, res) => {
 @param {string} flag
 @param {string} text
 */
-function compile(compiler, flag, text)
-{
+function compile(compiler, flag, text) {
 	/** @type {string} */
-	const fifoDir = '/tmp/__online_c_compiler__';
+	const fifoDir = "/tmp/__online_c_compiler__";
 	/** @type {string} */
-	const fifoFile = '__fifo__';
+	const fifoFile = "__fifo__";
 	mkfifo(fifoFile, fifoDir);
 	writeToFifo(text, `${fifoDir}/${fifoFile}`);
 	try {
 		cp.execSync(`<${fifoDir}/${fifoFile} ${compiler} ${flag} -Werror -fsyntax-only -x c -`);
 	} catch (error) {
-		return '<br>Compilation failed:<br>' + String(error.message)
-			// remove newlines
-			.replace(/\n/g, '')
-			// only show compiler warnings
-			.replace(/^.*?-fsyntax-only -x c -/, '')
-			// add newlines
-			.replace(/<stdin>:/g, '<br>');
+		return (
+			"<br>Compilation failed:<br>" +
+			String(error.message)
+				// remove newlines
+				.replace(/\n/g, "")
+				// only show compiler warnings
+				.replace(/^.*?-fsyntax-only -x c -/, "")
+				// add newlines
+				.replace(/<stdin>:/g, "<br>")
+		);
 	}
-	return 'Compiled successfuly!';
+	return "Compiled successfuly!";
 }
 
 /**
@@ -67,8 +69,7 @@ function compile(compiler, flag, text)
 @param {string} file
 @param {string} dir
 */
-function mkfifo(file, dir)
-{
+function mkfifo(file, dir) {
 	cp.execSync(`test -e ${dir} || mkdir ${dir}`);
 	/** @type {string} */
 	const fifoPath = `${dir}/${file}`;
@@ -80,7 +81,6 @@ function mkfifo(file, dir)
 @param {string} buf
 @param {string} path
 */
-function writeToFifo(buf, path)
-{
+function writeToFifo(buf, path) {
 	cp.execFile(`${__dirname}/bin/write_to_file`, [buf, path]);
 }
