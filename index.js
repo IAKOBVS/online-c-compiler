04434e916115eeae54b960a964ade554622cd951
James H <nus4xd@gmail.com>
2023-07-03 09:22:04 +0700

    newest

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const cp = require("child_process");
 const path = require("path");
 const fs = require("fs");
 const os = require("os");
+const str = require("./lib/str.js");
 /** @type {string} */
 const TMP_DIR = os.tmpdir();
 
@@ -52,7 +53,7 @@ function compile(compiler, flag, text) {
 	/** @type {string} */
 	const file_dir = path.join(TMP_DIR, "__tmp__");
 	/** @type {string} */
-	const file_path = mktemp(file_dir);
+	const file_path = str.mktemp(file_dir);
 	try {
 		fs.writeFileSync(file_path, text);
 		cp.execSync(`${compiler} ${flag} -Werror -fsyntax-only -x c ${file_path}`);
@@ -61,40 +62,10 @@ function compile(compiler, flag, text) {
 		error.message = error.message.substring(
 			error.message.indexOf(file_path) + file_path.length
 		);
-		error.message = replace_all(error.message, file_path + ":", "");
-		error.message = replace_all(error.message, "\n", "<br>");
+		error.message = str.replace_all(error.message, file_path + ":", "");
+		error.message = str.replace_all(error.message, "\n", "<br>");
 		return "<br>Compilation failed:<br>" + error.message;
 	}
 	fs.rmSync(file_path);
 	return "<br>Compiled successfuly!";
-}
-
-/**
-@param {string} dir
-@returns {string}
-*/
-function mktemp(dir) {
-	/** @type {string} */
-	let ret;
-	while (
-		(ret = Math.random().toString(36).slice(2, 7)) == "" ||
-		fs.existsSync(path.join(dir, ret))
-	);
-	return dir + ret;
-}
-
-/**
-@param {string} s
-@param {string} search
-@param {string} replace
-@returns {string}
-*/
-function replace_all(s, search, replace) {
-	/** @type {number} */
-	let old_len;
-	do {
-		old_len = s.length;
-		s = s.replace(search, replace);
-	} while (old_len != s.length);
-	return s;
 }
