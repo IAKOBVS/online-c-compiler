80ca30df2bdd4c1f43dfcc0513c49f0d59842609
James H <nus4xd@gmail.com>
2023-07-06 14:28:42 +0700

    newest

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ app.get("/", (req, res) => {
 app.post("/compile", (req, res) => {
 	/** @type {string} */
 	const flag = req.body.flag;
-	if (/[^- _+=0-9A-Za-z]/.test(flag)) {
+	if (/[^-\s_+=0-9A-Za-z]/.test(flag)) {
 		res.send(
-			"Passing illegal characters as flags!<br>Only use characters in [- _+=0-9A-Za-z]."
+			"Passing illegal characters as compiler flags!<br>Only use characters in [- _+=0-9A-Za-z]."
 		);
 		return;
 	}
@@ -39,6 +39,9 @@ app.post("/compile", (req, res) => {
 	const text = req.body.text;
 	if (!/\S/.test(text)) {
 		res.send("Source code is empty.");
+		return;
+	} else if (/(?:^|\n)\s*#\s*include\s*"/.test(text)) {
+		res.send("Trying to include headers from the filesystem:<br>#include \"header.h\".<br><br>Please use standard includes with angle brackets instead:<br>#include &ltheader.h&gt");
 		return;
 	}
 	/** @type {string} */
@@ -58,22 +61,28 @@ function compile(compiler, flag, text) {
 	const file_dir = path.join(TMPDIR, "__tmp__");
 	/** @type {string} */
 	const file_path = str.mktemp(file_dir);
+	const file_out = str.mktemp(file_dir);
 	try {
 		fs.writeFileSync(file_path, text);
 	} catch (error) {
 		return error.message;
 	}
 	try {
-		cp.execSync(`${compiler} ${flag} -Werror -fsyntax-only -x c ${file_path}`);
+		cp.execSync(`${compiler} ${flag} -Werror -x c ${file_path} -o ${file_out}`);
 	} catch (error) {
 		fs.rmSync(file_path);
 		/** @type {string} */
 		let err = error.message;
-		err = err.substring(err.indexOf(file_path) + file_path.length);
+		err = err.substring(err.indexOf(file_out) + file_out.length);
 		err = str.replace_all(err, file_path + ":", "");
 		err = str.replace_all(err, "\n", "<br>");
 		return "<br>Compilation failed:<br>" + err;
 	}
-	fs.rmSync(file_path);
+	try {
+		fs.rmSync(file_path);
+		fs.rmSync(file_out);
+	} catch (error) {
+		return error.message;
+	}
 	return "<br>Compiled successfuly!";
 }
